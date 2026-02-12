3fc86073d28722cd0a42d280910ac0feee550218
samri-Aa <samrawitalemshet1921@gmail.com>
2026-02-12 12:36:15 +0300

    Add functionality to convert string case based on counts

---
+++ Word.py
@@ -0,0 +1,12 @@
+s=input().strip()
+upper_count=0
+lower_count=0
+for char in s:
+    if char.isupper():
+        upper_count+=1
+    else:
+        lower_count+=1
+if upper_count>lower_count:
+    print(s.upper())
+else:
+    print(s.lower())
